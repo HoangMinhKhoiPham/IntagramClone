bb5c7c370387714ebe3927b37705e3c3a4eb9340
HoangMinhKhoiPham <97756628+HoangMinhKhoiPham@users.noreply.github.com>
2023-06-25 15:01:29 -0400

    Updatte

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1024px-Instagram_logo_2022.svg.png" class="rounded-circle" alt="", style="max-width: 200px;">
+        </div>
+    <div class="col-9 pt-5">
+        <div><h1>Intagram</h1>   </div>
+            <div class ="d-flex">
+                <div style="padding-right: 10px;"><strong>153</strong> posts</div>
+                <div style="padding-right: 10px;"><strong>23k</strong> followers</div>
+                <div style="padding-right: 10px;"><strong>212</strong> following </div>
             </div>
+            <div class="pt-4 font-weight-bold">Instagram.com</div>
+            <div>Instagram is available as a mobile app for iOS and Android devices, providing a visually-focused and user-friendly experience for sharing and discovering visual content.</div>
+        <div><a href="#">www.instagram.com</a></div>
+    </div>
+    </div>
+
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg" class="w-100">
         </div>
     </div>
 </div>
